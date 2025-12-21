e5d0f49beeb1405fefa15dffe19a3d79a7e4fd43
Andrei LAZAROV <andrei_lazarov@yahoo.com>
2025-12-21 16:42:10 +0200

    Revert converters

--- zigbee2mqtt/converters/tuya_with_ota.js
+++ zigbee2mqtt/converters/tuya_with_ota.js
@@ -26,14 +26,43 @@ const multiplePinoutsDescription = "WARNING! There are multiple known pinouts fo
 ********************************************************************/
 
 const tuyaModels = [
+    "FZB-1",
+    "QS-Zigbee-SEC01-U",
+    "QS-Zigbee-SEC02-U",
     "TS0001",
+    "TS0001_switch_1_gang",
+    "TS0001_switch_module",
+    "TS0001_switch_module_1",
     "TS0002",
     "TS0002_basic",
+    "TS0002_limited",
     "TS0003",
+    "TS0003_switch_3_gang",
+    "TS0003_switch_module_2",
     "TS0004",
+    "TS0004_switch_module",
+    "TS0004_switch_module_2",
+    "TS0011",
+    "TS0011_switch_module",
+    "TS0012",
+    "TS0012_switch_module",
+    "TS0013",
+    "TS0013_switch_module",
+    "TS0014",
+    "TS0044",
+    "TS011F_plug_1",
+    "TS011F_plug_2",
+    "TS0726_1_gang_scene_switch",
+    "TS0726_2_gang_scene_switch",
+    "TS0726_3_gang",
+    "WHD02",
+    "_TZ3000_pgq7ormg",
 ];
 
 const tuyaMultiplePinoutsModels = [
+    "TS0001_switch_module_1",
+    "TS0002_limited",
+    "TS0004_switch_module_2",
 ];
 
 for (let definition of tuyaDefinitions) {
@@ -60,6 +89,7 @@ for (let definition of tuyaDefinitions) {
 }
 
 const moesModels = [
+    "ZS-EUB_1gang",
 ];
 
 const moesMultiplePinoutsModels = [
@@ -89,6 +119,7 @@ for (let definition of moesDefinitions) {
 }
 
 const avattoModels = [
+    "LZWSM16-1",
 ];
 
 const avattoMultiplePinoutsModels = [
@@ -118,6 +149,7 @@ for (let definition of avattoDefinitions) {
 }
 
 const girierModels = [
+    "JR-ZDS01",
 ];
 
 const girierMultiplePinoutsModels = [
